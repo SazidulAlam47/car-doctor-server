2bd45bd29f82fb68605cdf0e23caef75bc0e000b
Sazidul Alam <sazidulalam20@gmail.com>
2024-05-08 23:59:09 +0600

    deploy

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ const verifyToken = async (req, res, next) => {
 async function run() {
     try {
         // Connect the client to the server (optional starting in v4.7)
-        await client.connect();
+        // await client.connect();
 
         const database = client.db("CarDoctorDB");
         const serviceCollection = database.collection("services");
